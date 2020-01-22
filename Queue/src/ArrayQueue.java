018cd41199a844d4b16dd36930f0a0e87910218c
Zu3zz <zthzththth@163.com>
2020-01-22 16:27:54 +0800

    feat: finish ArrayQueue

---
+++ Queue/src/ArrayQueue.java
@@ -0,0 +1,68 @@
+/**
+ * Date: 2020/1/22 4:11 下午
+ *
+ * @author 3zZ.
+ */
+public class ArrayQueue<E> implements Queue<E> {
+    private Array<E> array;
+
+    public ArrayQueue(int capacity) {
+        array = new Array<>(capacity);
+    }
+
+    public ArrayQueue() {
+        array = new Array<>();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Queue: ");
+        stringBuilder.append("Front [");
+        for (int i = 0; i < array.getSize(); i++) {
+            stringBuilder.append(array.get(i));
+            if (i != array.getSize() - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("] Tail.");
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        for (int i = 0; i < 10; i++) {
+            arrayQueue.enqueue(i);
+            System.out.println(arrayQueue);
+            if (i % 3 == 2){
+                arrayQueue.dequeue();
+                System.out.println(arrayQueue);
+            }
+        }
+    }
+}
