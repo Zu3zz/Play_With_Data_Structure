a3e9f02416dbe252270227441c689e1b5a902819
Zu3zz <zthzththth@163.com>
2020-01-27 19:28:23 +0800

    feat: finish stack with linkedlist

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,14 +1,43 @@
-import java.util.Stack;
+import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<Integer>();
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-            System.out.println(stack);
+    /**
+     * 测试使用stack运行opCount个push和pop操作所需要的时间，单位：秒
+     *
+     * @param stack   需要进行比较的栈的实现形式
+     * @param opCount 运行的论述
+     * @return 消耗的总时间
+     */
+    private static double testStack(Stack<Integer> stack, int opCount) {
+
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
         }
-        stack.pop();
-        System.out.println(stack);
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+
+        int opCount = 100000;
+
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + " s");
+
+        // 其实这个时间比较很复杂，因为LinkedListStack中包含更多的new操作
     }
 }
