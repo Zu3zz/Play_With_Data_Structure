70fc6bce2d5e8da8a9b4b3a1af7c71673e21c8a6
Zu3zz <zthzththth@163.com>
2020-02-03 23:29:51 +0800

    feat: finish Heap based on Array

---
+++ MaxHeap/src/MaxHeap.java
@@ -0,0 +1,121 @@
+/**
+ * Date: 2020/2/2 9:36 下午
+ *
+ * @author 3zZ.
+ */
+public class MaxHeap<E extends Comparable<E>> {
+    private Array<E> data;
+
+    public MaxHeap(int capacity) {
+        data = new Array<>(capacity);
+    }
+
+    public MaxHeap(E[] arr) {
+        data = new Array<>(arr);
+        for (int i = parent(arr.length - 1); i >= 0; i--) {
+            shiftDown(i);
+        }
+    }
+
+    public MaxHeap() {
+        data = new Array<>();
+    }
+
+    public int size() {
+        return data.getSize();
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    /**
+     * 返回父亲节点的索引是谁
+     *
+     * @param index 需要查询的节点
+     * @return 父亲节点的索引
+     */
+    private int parent(int index) {
+        if (index == 0) {
+            throw new IllegalArgumentException("index-0 doesn't have any parent");
+        }
+        return (index - 1) / 2;
+    }
+
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+
+    /**
+     * 向堆中添加元素
+     *
+     * @param e 需要添加的元素
+     */
+    public void add(E e) {
+        data.addLast(e);
+        shiftUp(data.getSize() - 1);
+    }
+
+    /**
+     * 将添加到末尾的元素交换到它应该到的位置
+     *
+     * @param k 新添加元素的下标
+     */
+    private void shiftUp(int k) {
+        while (k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0) {
+            data.swap(k, parent(k));
+            k = parent(k);
+        }
+    }
+
+    /**
+     * 看一下堆中的最大元素
+     *
+     * @return 返回堆中的最大元素
+     */
+    public E findMax() {
+        if (data.getSize() == 0) {
+            throw new IllegalArgumentException("Can not findMax when heap is empty.");
+        }
+        return data.get(0);
+    }
+
+    /**
+     * 取出堆中最大元素
+     *
+     * @return 返回堆中最大的元素
+     */
+    public E extractMax() {
+        E ret = findMax();
+        data.swap(0, data.getSize() - 1);
+        data.removeLast();
+        shiftDown(0);
+        return ret;
+    }
+
+    private void shiftDown(int k) {
+        while (leftChild(k) < data.getSize()) {
+            int j = leftChild(k);
+            if (j + 1 < data.getSize() && data.get(j + 1).compareTo(data.get(j)) > 0) {
+                j = rightChild(k);
+                // data[j] 是 leftChild 和 rightChild 中的最大值
+            }
+            if (data.get(k).compareTo(data.get(j)) >= 0) {
+                break;
+            }
+            data.swap(k, j);
+            k = j;
+        }
+    }
+
+    public E replace(E e) {
+        E ret = findMax();
+        data.set(0, e);
+        shiftDown(0);
+        return ret;
+    }
+}
---
+++ MaxHeap/src/Queue.java
@@ -0,0 +1,43 @@
+/**
+ * Date: 2020/2/3 11:29 下午
+ *
+ * @author 3zZ.
+ */
+public interface Queue<E> {
+
+    /**
+     * 返回stack的大小
+     *
+     * @return stack的大小
+     */
+    int getSize();
+
+    /**
+     * 返回stack是否为空
+     *
+     * @return true为空 false为非空
+     */
+    boolean isEmpty();
+
+    /**
+     * 向queue顶部压入一个元素
+     *
+     * @param e 需要push的元素值
+     */
+    void enqueue(E e);
+
+    /**
+     * 取当前队列顶元素
+     *
+     * @return 栈顶元素的值
+     */
+    E getFront();
+
+    /**
+     * 出队操作
+     *
+     * @return 队列顶元素的值
+     */
+    E dequeue();
+}
+
